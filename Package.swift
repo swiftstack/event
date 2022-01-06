6123b5dbe101ed319122d002a866ef9894f06e09
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-01-06 08:29:50 +0300

    [Package] Add minimum target requirements

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "Event",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+    ],
     products: [
         .library(
             name: "Event",
